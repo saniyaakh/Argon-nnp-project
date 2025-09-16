806f362c05c8f492aa8ef4cff0b461d55ab5d961
saniyaakh <95710510+saniyaakh@users.noreply.github.com>
2025-09-16 16:58:50 +0100

    Delete replace_symfuncs.sh

--- replace_symfuncs.sh
+++
@@ -1,9 +0,0 @@
-line_number=$(grep -wn EXAMPLE $2 | cut -d : -f 1)
-line_number=$(($line_number+2))
-head -n $line_number $2
-sed -i "${line_number},\$d" $2
-
-cat $1 >> $2
-echo "Files copied!"
-sed -i 's/H/Si/g' $2
-echo "Symbols changed!"
